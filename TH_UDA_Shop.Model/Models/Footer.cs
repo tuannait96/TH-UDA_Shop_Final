3159ab1c4b57c61d69f149036cda966ac6815950
DESKTOP-J87UH19\KA <tuanna.it96@gmail.com>
2018-09-05 16:07:22 +0700

    model

---
+++ TH_UDA_Shop.Model/Models/Footer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH_UDA_Shop.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+}
---
+++ TH_UDA_Shop.Model/Models/MenuGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH_UDA_Shop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+
+    }
+}
